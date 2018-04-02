f62cd783dbe3dcbb53c44d0689d05b3fe247c06f
MichaelYin <hgyinjian@gmail.com>
2018-04-02 11:46:34 +0800

    init commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $('.parallax').parallax();
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+    pressed: true
+  });
+
+  $('.sidenav').sidenav();
+
+});
